c828bb083b89d180ec788b3d21f18aa730def1ee
Judith Elaine Bush <judielaine@users.noreply.github.com>
2015-05-21 15:31:16 -0700

    Python template style fix
    Looks like markdown & URLs is too long for the 72 char guideline.

--- template.py
+++ template.py
@@ -1,15 +1,19 @@
 #
 # template.py 
 #
-# A record of learned best practices informed first by PEP 0008.
+# A record of best-practices informed first by PEP 0008.
 # 
 # Judith E. Bush
-# Time-stamp: "2015-05-21 14:48:46 bushj"
+# Time-stamp: "2015-05-21 15:30:00 bushj"
 #
-# References: [PEP 0008 Style Guide for Python Code](https://www.python.org/dev/peps/pep-0008/)
-#             [PEP 0257 Docstring Conventions](https://www.python.org/dev/peps/pep-0257/)
+# References:
+# PEP 0008 Style Guide for Python Code
+#     https://www.python.org/dev/peps/pep-0008/
+# PEP 0257 Docstring Conventions
+#     https://www.python.org/dev/peps/pep-0257/
+# Emacs-for-Python workflow
+#     https://github.com/gabrielelanaro/emacs-for-python/wiki/workflow
 #
-
 
 # Standard library imports ---------------------------------------------#------#
 
